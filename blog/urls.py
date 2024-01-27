60bdd8f61d8e17ae5f05d97f700b53408303d2c0
temur <aliyer.temur95@gmail.com>
2024-01-27 16:27:29 +0500

    news pro

---
+++ blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import post_share, post_detail, post_list, post_comment
+
+app_name = 'blog'
+
+
+
+urlpatterns = [
+# post views
+#     path('', PostListView.as_view(), name='post_list'),
+    path('', post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/',
+         post_list, name='post_list_by_tag'),
+    # path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail,
+         name='post_detail'),
+    path('<int:post_id>/share/',
+         post_share, name='post_share'),
+    path('<int:post_id>/comment/',
+         post_comment, name='post_comment'),
+
+]
