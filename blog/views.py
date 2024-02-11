16090c0746c6bc5184e17e85f54f61b19d7c5189
temur <aliyer.temur95@gmail.com>
2024-02-12 02:31:38 +0500

    delete code

--- blog/views.py
+++ blog/views.py
@@ -61,21 +61,6 @@ def post_detail(request,  year, month, day, post):
                                               'similar_posts': similar_posts})
 
 
-# def post_share(request, post_id):
-# # Retrieve post by id
-#     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
-
-#     if request.method == 'POST':
-#         # Form was submitted
-#         form = EmailPostForm(request.POST)
-#         if form.is_valid():
-#             # Form fields passed validation
-#             cd = form.cleaned_data
-#         # ... send email
-#     else:
-#         form = EmailPostForm()
-#     return render(request, 'post/share.html', {'post': post,'form': form})
-#
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
